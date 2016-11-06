dcb68f6b344f94f4aa2739e20a26e2af4fd7b74e
Chad Kreutzer <kanthalion@gmail.com>
2016-11-06 13:41:33 +0000

    done with 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,33 @@
 #write your code here
+
+def add augend, addend
+    augend + addend
+end
+
+def subtract minuend, subtrahend
+    minuend - subtrahend
+end
+
+def sum array
+    if array == []
+        0
+    else
+        array.inject { |sum, n| sum + n }
+    end
+end
+
+def multiply *args
+    args.reduce(:*)
+end
+
+def power base, exponent
+    base**exponent
+end
+
+def factorial number
+    if number == 0
+        1
+    else
+        (1..number).reduce(:*)
+    end
+end
