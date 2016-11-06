e175eeb16d8b3ce566eb5ec2dbe0a5baefa4dcbc
ChadKreutzer <kanthalion@gmail.com>
2016-11-06 15:51:07 -0700

    done through start_of_word in 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,20 @@
 #write your code here
+
+def echo input
+  input
+end
+
+def shout input
+  input.upcase
+end
+
+def repeat input, *args
+  temp_array = []
+  reps = args == [] ? 2 : args.first.to_i
+  (0...reps).each { temp_array.push(input) }
+  temp_array.join(' ')
+end
+
+def start_of_word input, range
+  input[0, range]
+end
