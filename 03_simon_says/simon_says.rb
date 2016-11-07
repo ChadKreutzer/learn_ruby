09fbd20802033b4fb24d7a901bcac8ce07d7d1db
ChadKreutzer <kanthalion@gmail.com>
2016-11-06 23:23:26 -0700

    done through 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -18,3 +18,21 @@ end
 def start_of_word input, range
   input[0, range]
 end
+
+def first_word input
+  /\w*/.match(input).to_s
+end
+
+def titleize input
+  # and over the
+  temp = input.split
+  temp.map! do |x|
+    if /(and|over|the)/.match(x) == nil
+      x.capitalize
+    else
+      x
+    end
+  end
+  temp[0].capitalize!
+  temp.join(' ') 
+end
