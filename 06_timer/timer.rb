9ba3be8c097b2aa0aa1d6501a8f64bb5dbd04628
ChadKreutzer <kanthalion@gmail.com>
2016-11-07 14:25:19 -0700

    complete learn_ruby

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def padded num
+      num < 10 ? '0' + num.to_s : num.to_s
+  end
+
+  def time_string    
+    hours = (@seconds / 3600)
+    minutes = ((@seconds - hours * 3600) / 60)
+    seconds = (@seconds - hours * 3600 - minutes * 60)
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"      
+  end
 end
