38b11033c62f341c87a51efcb454ed8685df3196
ChadKreutzer <kanthalion@gmail.com>
2016-11-07 14:44:14 -0700

    added bonus challenges to 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -6,10 +6,12 @@ def translate input
   def pigize word
     beg = word[/\b([^aeiou]|[qu])+/].to_s
     pig = word[beg.length, word.length - beg.length] + beg + 'ay'
-    pig
+    word == word.capitalize ? pig.capitalize : pig
   end
 
   input.split.map do |w|
-    pigize(w)
+    punctuation = w[/\W$/].to_s
+    w = punctuation != '' ? w[0...-1] : w 
+    pigize(w) + punctuation
   end.join(' ')
 end
