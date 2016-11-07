206b859669275ed31257c0604592e887e4551851
ChadKreutzer <kanthalion@gmail.com>
2016-11-07 13:39:20 -0700

    Done through 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
 # write your code here
+
+  def title
+    @title
+  end
+
+  def title=(input)
+    temp = input.split
+    small_words = /(\ba\b|\ban\b|\band\b|\bin\b|\bof\b|\bthe\b)/
+    temp.map! do |x|
+      if small_words.match(x) == nil
+        x.capitalize
+      else
+        x
+      end
+    end
+    temp[0].capitalize!
+    @title = temp.join(' ')    
+  end
 end
