caba6ede746fc346a9002f5faba8ebc5cd1390d8
ChadKreutzer <kanthalion@gmail.com>
2016-11-07 08:39:42 -0700

    done through 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -2,5 +2,14 @@
 # regex = \b([^aeiou]|[qu])+
 
 def translate input
-  
+
+  def pigize word
+    beg = word[/\b([^aeiou]|[qu])+/].to_s
+    pig = word[beg.length, word.length - beg.length] + beg + 'ay'
+    pig
+  end
+
+  input.split.map do |w|
+    pigize(w)
+  end.join(' ')
 end
