5d09fae6cf7e9cdb79342441cc9046dbdd08e325
Chad Kreutzer <kanthalion@gmail.com>
2016-11-20 10:47:44 +0000

    refactor book title

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,21 +1,22 @@
 class Book
-# write your code here
-
-  def title
-    @title
-  end
+  attr_reader :title
 
   def title=(input)
-    temp = input.split
-    small_words = /(\ba\b|\ban\b|\band\b|\bin\b|\bof\b|\bthe\b)/
-    temp.map! do |x|
-      if small_words.match(x) == nil
-        x.capitalize
-      else
-        x
-      end
-    end
-    temp[0].capitalize!
-    @title = temp.join(' ')    
+    title_array = input.capitalize.split
+    
+    @title = title_array.map{|word| capitalize_non_small word}.join(' ')
   end
+  
+  private
+  Small_words = /\b(an?d?|in|of|over|the)\b/
+  
+  def capitalize_non_small test_word
+    
+    if Small_words.match(test_word)
+      test_word
+    else
+      test_word.capitalize
+    end
+  end
+  
 end
